9eea263524f856c1273480c3cc1aa6573efe2fa3
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-01-29 00:46:19 +1100

    testing deployment to heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {  // route handler watching for a very specific http method
-    res.send({hi: 'there'});
+    res.send({hi: 'buddy'});
 });
 
 const PORT = process.env.PORT || 5000;
