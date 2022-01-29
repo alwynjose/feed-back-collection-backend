b2a19f2188388e5174c8bc6a872b8494788d2558
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-01-29 22:47:12 +1100

    setting up google strategy

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {  // route handler watching for a very specific http method
-    res.send({hi: 'buddy'});
-});
+/**
+ * @description new GoogleStrategy creates a new instance of the google passport strategy
+ */
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+        console.log(accessToken);
+        }
+    )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
