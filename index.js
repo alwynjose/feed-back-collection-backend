fa1fbd74e1db309dc02992bd31cf62cff53de0c8
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-02-03 08:25:57 +1100

    Added cookie-session
    - express uses cookies inside application
    - passport uses cookies to handle auth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport'); // the services/passport.js is not returning anything so we don't have to assign it to anything like const passport = require(...)
@@ -7,7 +9,20 @@ require('./services/passport'); // the services/passport.js is not returning any
 // const authRoutes = require('./routes/authRoutes'); // refactored id1
 
 mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+//tell express to make use of cookie inside our application
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000, // milliseconds - 30 days
+        keys: [keys.cookieKey]
+    })
+);
+
+// tell passport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // authRoutes(app); // refactored id1
 require('./routes/authRoutes')(app); // refactored id1
