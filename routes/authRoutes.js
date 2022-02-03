1e46938b92c0f91268605d09d2f14d2d2d77c6be
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-02-03 13:35:43 +1100

    logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,14 @@ module.exports = app => {
     })
     );
     
+app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+});
+
     app.get('/auth/google/callback', passport.authenticate('google'));
+    // get request to app
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
 }
