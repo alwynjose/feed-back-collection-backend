5744d249575b582c859779596b35eea57de1a069
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-02-13 16:28:58 +1100

    setup redux , react-redux
    - deleted some files from src folder
    - installed redux, react-redux
    - created reducers (authReducers)

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'; // using 'import' of webpack and babel giving easy access to es2015 modules
+// import { BrowserRouter, Router } from 'react-router-dom';
+
+// const Header = () => <h2>Header</h2>
+// const Dashboard = () => <h2>Dashboard</h2>
+// const SurveyNew = () => <h2>SurveyNew</h2>
+// const Landing = () => <h2>Landing</h2>
+
+const App = () => { // functional component 
+    return ( // returns jsx
+        <div>
+            Hi There!
+        </div>
+    );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from  './components/App';
+import reducers from './reducers';
+
+const store = createStore(() => [], {}, applyMiddleware()); // args > reducer, initial state of application, middleware call redux chunk
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<Provider store={store}><App /></Provider>,
+document.querySelector('#root'));  // takes two args - router component and where its going to render the component inside DOM
+                        // <App /> -> jsx tags because react.DOM render expects a component instance which we creat by jsx tags
