b39aa23ce19e3129319addbe1fdbee41feb8e46a
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-02-23 18:21:17 +1100

    header import

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'; // using 'import' of webpack and babel giving easy access to es2015 modules
 import { BrowserRouter, Route } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>;
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
