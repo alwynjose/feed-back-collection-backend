69e5c1b3101a959f8ece560f3b8e0f37acefd47e
Alwyn Jose <alwynjose@users.noreply.github.com>
2022-02-23 17:51:11 +1100

    react router in place

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react'; // using 'import' of webpack and babel giving easy access to es2015 modules
-// import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-// const Header = () => <h2>Header</h2>
-// const Dashboard = () => <h2>Dashboard</h2>
-// const SurveyNew = () => <h2>SurveyNew</h2>
-// const Landing = () => <h2>Landing</h2>
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => { // functional component 
     return ( // returns jsx
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact={true} path="/" component={Landing} />
+                    <Route exact={true} path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
